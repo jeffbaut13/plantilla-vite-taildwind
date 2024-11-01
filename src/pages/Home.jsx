e9e0ecc5dbeb12582e59b391b3e9815a5fb781e0
jeffbaut13 <jeferssonabv@gmail.com>
2024-11-01 11:46:56 -0500

    store zustand

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import useCounterStore from '../store/store';
+
 export const Home = () => {
+
+  const { count, increase, decrease, reset } = useCounterStore();
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Contador: {count}</h1>
+      <button onClick={increase}>Incrementar</button>
+      <button onClick={decrease}>Decrementar</button>
+      <button onClick={reset}>Reiniciar</button>
+    </div>
   )
 }
